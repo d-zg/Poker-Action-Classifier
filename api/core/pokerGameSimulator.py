3eda1ea89b2130d87ac0dee6992317e50bc4db5a
Daniel Zhang <danielzhang139@gmail.com>
2022-06-03 21:15:43 -0400

    writes input output pairs with state & action to csv. current features: amount to play, previous bet, last action, position, pot size, stack, equity vs unknown.

--- api/core/pokerGameSimulator.py
+++ api/core/pokerGameSimulator.py
@@ -1,6 +1,9 @@
+import csv
 from queue import Queue
 from typing import List, Dict
 from pokerface import *
+from csv import *
+
 
 class Game:
     # def __new__(cls):
@@ -16,13 +19,15 @@ class Game:
         self.runningEventLoop : bool = True
         self.dealer = Dealer(players, big_blind, small_blind)
         self.history = Queue()
-    def __registerPlayer(self, newPlayer):
+        self.bankruptPlayers = []
+    def registerPlayer(self, newPlayer):
         if (not newPlayer.id in self.allPlayers):
             self.allPlayers.append(newPlayer)
+            self.dealer.players.append(newPlayer)
             newPlayer.enterGame(self)
         else:
             print("\nError, player already registered.")
-    def __removePlayer(self, id):
+    def removePlayer(self, id):
         self.allPlayers[id].leaveGame
         del self.allPlayers[id]
     def __runEventLoop(self):
@@ -38,16 +43,33 @@ class Game:
     def foldPlayer(self, player):
         self.dealer.foldPlayer(player)
         self.history.put("Player " + player.id + " folded.")
+        player.lastAction = "Fold"
     def raisePlayer(self, player, raiseAmount):
         self.dealer.raisePlayer(player, int(raiseAmount))
         self.history.put("Player " + player.id + " raised.")
+        player.lastAction = "Raise"
+    def raisePlayerThird(self, player):
+        self.dealer.raisePlayer(player, self.dealer.pot/3)
+        self.history.put("Player " + player.id + " raised 1/3rd of the pot.")
+        player.lastAction = "Raise third pot"
+    def raisePlayerPot(self, player):
+        self.dealer.raisePlayer(player, self.dealer.pot)
+        self.history.put("Player " + player.id + " raised by the pot size.")
+        player.lastAction = "Raise pot"
+    def raisePlayerAllIn(self, player):
+        self.dealer.raisePlayer(player, player.stack)
+        self.history.put("Player " + player.id + " raised all in.")
+        player.lastAction = "All in"
     def checkPlayer(self, player):
         self.dealer.checkPlayer(player)
         self.history.put("Player " + player.id + " checked.")
+        player.lastAction = "Check"
     def callPlayer(self, player):
         self.dealer.callPlayer(player)
         self.history.put("Player " + player.id + " called.")
-    
+        player.lastAction = "Call"
+    def getEquity(self, player):
+        self.dealer.getPlayerEquity(player)
 
 
 class Player:
@@ -57,12 +79,14 @@ class Player:
         self.currentBet : int = 0
         self.currentGame : Game = None
         self.hand = []
+        self.lastAction = None
     def enterGame(self, instance):
         self.currentGame = instance
         # print some recognition that you were entered into this game
     def leaveGame(self):
         self.currentGame = None
     def getAction(self, pot, highestBet, lastRaisedID):
+        data = self.currentGame.dealer.getPreActionState(self)
         self.promptAction(pot, highestBet, lastRaisedID)
         initialInput = input("Enter your action: ")
         processedInput = initialInput.split(' ')
@@ -70,20 +94,40 @@ class Player:
         if len(processedInput) > 1:
             parameters = processedInput[1]
         if (commandInput == "fold"):
+            data.append("Fold")
             self.currentGame.foldPlayer(self)
         elif (commandInput == "raise"):
             if (not len(processedInput) > 1):
                 print("invalid raise, need amount.")
                 self.getAction(pot, highestBet, lastRaisedID)
+            elif (parameters == "1/3"):
+                data.append("Raise one third pot")
+                self.writeInputOutputPairs(data)
+                self.currentGame.raisePlayerThird(self)
+            elif (parameters == "pot"):
+                data.append("Raise pot")
+                self.currentGame.raisePlayerPot(self)
+            elif (parameters == "allin"):
+                data.append("Raise all in")
+                self.currentGame.raisePlayerAllIn(self)
             else :
                 self.currentGame.raisePlayer(self, parameters)
         elif (commandInput == "check"):
+            data.append("Check")
             self.currentGame.checkPlayer(self)
         elif (commandInput == "call"):
+            data.append("Call")
             self.currentGame.callPlayer(self)
+        elif (commandInput == "equity"):
+            self.currentGame.getEquity(self)
         else :
             print("That's not a valid input. Folding " + self.id)
             self.currentGame.foldPlayer(self)
+    def writeInputOutputPairs(self, data):
+        with open('danielActions.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Amount to play', 'Previous Bet', 'Last Action', 'Position', 'Pot Size', 'stack', 'Equity vs Unknown', 'Action'])
+            writer.writerow(data)
     def promptAction(self, pot, highestBet, lastRaisedID):
         print("Current pot is " + str(pot))
         print("\nYou, " + self.id + " are in for " + str(self.currentBet))
@@ -133,11 +177,23 @@ class Dealer:
         received_big = self.players[self.currentBlind].changeStack(-(self.big_blind))
         self.players[self.currentBlind].currentBet = received_big
         print("Collected " + str(received_big) + " as big blind from " + self.players[self.currentBlind].id)
+        self.players[self.currentBlind].lastAction = "Posted big"
         self.pot += received_big
         received_small = self.players[self.currentBlind - 1].changeStack(-(self.small_blind))
         self.players[self.currentBlind - 1].currentBet = received_small
         print("Collected " + str(received_small) + " as small blind from " + self.players[self.currentBlind - 1].id)
+        self.players[self.currentBlind - 1].lastAction = "Posted small"
         self.pot += received_small
+
+    def getPreActionState(self, player):
+        amountToPlay = self.currentBet - player.currentBet
+        potSize = self.pot
+        stack = player.stack
+        previousBet = player.currentBet
+        lastAction = player.lastAction
+        equityVsUnknown = self.getPlayerEquity(player) 
+        position = self.players.index(player) - self.big_blind # right now, headsup, so just returns if small or big. Change this for more players
+        return [amountToPlay, previousBet, lastAction, position, potSize, stack, equityVsUnknown]
 
     def foldPlayer(self, player):
         if (player in self.players and not player in self.foldedPlayers):
@@ -210,7 +266,8 @@ class Dealer:
         # folding places you in the folded category. Calling, raising, and checking all put in the IN category.
         # if you raise, you put everyone who was IN in the undecided category.
         # if you've spent all your money and haven't folded, you don't need to make decisions and are in for the rest of the hand.
-        while (len(self.foldedPlayers) + len(self.calledPlayers) + len(self.allInnedPlayers) != len(self.players) and len(self.allInnedPlayers) + 1 != len(self.players)):
+        # also, if everyone is all inned but one person, then we don't care about that person's action bc no one needs to respond.
+        while (len(self.foldedPlayers) + len(self.calledPlayers) + len(self.allInnedPlayers) != len(self.players) and len(self.allInnedPlayers) + 1 != len(self.players)): 
             if (not self.players[currentPosition] in self.foldedPlayers):
                 if (self.players[currentPosition].stack == 0):
                     self.allInnedPlayers.append(self.players[currentPosition]) if not self.players[currentPosition] in self.allInnedPlayers else None
@@ -225,9 +282,11 @@ class Dealer:
         return False
     
     def playPreFlop(self):
+        for player in self.players:
+            player.lastAction = None
         self.collectBlinds()
         self.dealHoleCards()
-        return self.runBettingLoop(self.currentBlind + 1)
+        return self.runBettingLoop((self.currentBlind + 1) % (len(self.players)))
 
     def playFlop(self):
         print("\nThe flop comes out as: ") # actually write the code to display this
@@ -266,7 +325,9 @@ class Dealer:
             gameOver = self.playRiver()
         if (not gameOver):
             for player in self.determineWinner():
-                print(player.id)
+                print("\n" + player.id + " wins the hand with " + str(player.hand))
+                self.winPot(player) # this is wrong, change it so that each player wins 1/nth the pot
+        self.communityCards.clear()
 
     def dealToPlayer(self, player):
         if player in self.players:
@@ -291,31 +352,37 @@ class Dealer:
                 bestHands.append(player)
         return bestHands
 
-        
+    def getPlayerEquity(self, player): # monte carlo method for getting player equity w/ 2500 sims
+        evaluator = StandardEvaluator()
+        riversWon = 0
+        for i in range(2500):
+            potentialDeck = StandardDeck()
+            potentialDeck.draw(player.hand)
+            potentialDeck.draw(self.communityCards)
+            opponentHand = potentialDeck.draw(2)
+            potentialRiver = self.communityCards + potentialDeck.draw(5 - len(self.communityCards))
+            if (evaluator.evaluate_hand(player.hand, potentialRiver) >= evaluator.evaluate_hand(opponentHand, potentialRiver)):
+                riversWon = riversWon + 1
+        print(riversWon/2500)
+        return riversWon/2500
+
+    def playGame(self):
+        self.inactivePlayers.clear()
+        while (len(self.inactivePlayers) + 1 < len(self.players)):
+            self.playRound()
+            for player in self.players:
+                if player.stack == 0: 
+                    self.inactivePlayers.append(player)
+        remainingPlayers = list(set(self.players) - set(self.inactivePlayers))
+        print(remainingPlayers[0].id + " wins.")
+
         
 player1 = Player(1000, "Warringloser")
 player2 = Player(2000, "The goat")
 
 test = Game(10, 5, [player1, player2])
 
-# test.foldPlayer(player1)
+test.dealer.playGame()
 
-
-# test.callPlayer(player2)
-
-# print(str(player1.stack))
-
-# test.dealer.collectBlinds()
-
-# print(str(player1.stack))
-# print(str(player2.stack))
-
-# test.dealer.winPot(player1)
-
-# print(str(player1.stack))
-
-test.dealer.playRound()
-
-# print(test.dealer.foldedPlayers[0].id)
-
-# print(player1.stack)
+# for historymessage in iter(test.history.get, None):
+#     print(historymessage)
