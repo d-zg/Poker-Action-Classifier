30a9d7e5bb2de32d178d70cb693d7db7ab01fadb
Daniel Zhang <danielzhang139@gmail.com>
2022-07-30 10:20:11 -0400

    Updates

---
+++ precache-manifest.6ad9499eeaec75933b9364a8ab4491b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828a699b58e13ad552b4842d51e04fb2",
+    "url": "/react-poker-calculator/index.html"
+  },
+  {
+    "revision": "379f4a77117600e466dd",
+    "url": "/react-poker-calculator/static/css/main.014842fc.chunk.css"
+  },
+  {
+    "revision": "20e49926fef958bc36c6",
+    "url": "/react-poker-calculator/static/js/2.c47ef7e0.chunk.js"
+  },
+  {
+    "revision": "379f4a77117600e466dd",
+    "url": "/react-poker-calculator/static/js/main.6e7edf33.chunk.js"
+  },
+  {
+    "revision": "180bc8d0ba53e29c1d57",
+    "url": "/react-poker-calculator/static/js/runtime-main.67286e3d.js"
+  }
+]);
